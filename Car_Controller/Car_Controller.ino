cb8aab754e5cb06ab2f4c73b8b09894640a6ae36
Harry C <124547151+harryc73@users.noreply.github.com>
2024-08-07 22:06:00 +1000

    Fixed Changes

--- Car_Controller/Car_Controller.ino
+++ Car_Controller/Car_Controller.ino
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 #include "LineSensor.h"
 #include "Motor.h"
 #include "CarController.h"
@@ -16,13 +15,7 @@ LineSensor lineSensor(LEFT_LINE_PIN, RIGHT_LINE_PIN);
 Motor motor(LEFT_SERVO_PIN, RIGHT_SERVO_PIN);
 
 CarController carController(lineSensor, motor);
-=======
-#include "CarController.h"
-#include "LineSensor.h"
-#include "Motor.h"
 
-#define LEFT_SERVO_PIN 
->>>>>>> Stashed changes
 
 void setup() {
   motor.init();
