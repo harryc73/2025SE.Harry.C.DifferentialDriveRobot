6194e4970626bbfa8d6a75c637f45fdc52f18294
Harry C <124547151+harryc73@users.noreply.github.com>
2024-08-08 21:38:42 +1000

    Added Notes

--- Car_Controller/CarController.h
+++ Car_Controller/CarController.h
@@ -13,7 +13,9 @@ class CarController
 {
 
 private:
-
+  // the reason why there is pointers instead of a simple address is because you can't copy the object from the old class, and also you can't initialise 
+  // right when the ControllerCar object is constructed, the pointers allow for intialisation to not happen immedeadtly and it to be later assigned
+  // this is the reason why you can use the methods of other classes and their objects, as it assigns memory to it without immedeatly initialising
   LineSensor* lineSensor;
   Motor* motor;
   LedArray* VersionStatus;
