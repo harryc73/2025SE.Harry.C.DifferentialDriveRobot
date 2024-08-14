9719165afec0ec58c17fa93a67ba2b900c69a4bb
harryc73 <harry.connorsle@education.nsw.gov.au>
2024-08-14 12:57:02 +1000

    Sharp Turn Changes

---
+++ Car_Controller_Sharp_Turn_Try/Car_Controller_PostDelay.ino
@@ -0,0 +1,27 @@
+#include "LineSensor.h"
+#include "Motor.h"
+#include "CarController.h"
+#include "LedArray.h"
+#include <Servo.h>
+
+#define LEFT_SERVO_PIN 9
+#define RIGHT_SERVO_PIN 10
+
+#define LEFT_LINE_PIN 2
+#define RIGHT_LINE_PIN 3
+
+static const char* VersionNum = "V00.00.01";
+
+LineSensor lineSensor(LEFT_LINE_PIN, RIGHT_LINE_PIN);
+Motor motor(LEFT_SERVO_PIN, RIGHT_SERVO_PIN);
+LedArray VersionStatus(VersionNum);
+CarController carController(&lineSensor, &motor, &VersionStatus);
+
+
+void setup() {
+  carController.init();
+}
+
+void loop() {
+  carController.followLine();
+}
---
+++ Car_Controller_Sharp_Turn_Try/LineSensor.cpp
@@ -0,0 +1,76 @@
+#include "LineSensor.h"
+
+// if they both on the line both the analog 31 for left, 32 for right
+//If the left is off the line it sits at 31-31, and right sits above 40
+// if the right is off the line it sits at 31-32, and left is above 40
+
+LineSensor::LineSensor(byte leftPin, byte rightPin)
+{
+  this->_leftPin = leftPin;
+  this->_rightPin = rightPin;
+}
+
+void LineSensor::init()
+{
+  pinMode(_leftPin, INPUT);
+  pinMode(_rightPin, INPUT);
+  }
+
+int LineSensor::readLeftState()
+{
+  leftState = analogRead(_leftPin);
+  return leftState;
+}
+
+int LineSensor::readRightState()
+{
+  rightState = analogRead(_rightPin);
+  return rightState;
+}
+
+
+byte LineSensor::readState()
+{
+  leftState = analogRead(_leftPin);
+  rightState = analogRead(_rightPin);
+}
+
+bool LineSensor::isLeftLine()
+{
+  readState();
+  if(leftState == HIGH)
+    return true;
+  else
+    return false;
+}
+
+bool LineSensor::isRightLine()
+{
+  readState();
+  if(rightState == HIGH)
+    return true;
+  else
+    return false;
+}
+
+
+String LineSensor::getState()
+{
+  readState();
+
+  String state = "Left: "; // String so it can be a string
+
+  if(leftState == HIGH)
+    state += "No Line Detected";
+  else
+    state += "Line Detected";
+
+  state += " Right: ";
+  
+  if(rightState == HIGH)
+    state += "No Line Detected";
+  else
+    state += "Line Detected";
+  
+  return state;
+}
