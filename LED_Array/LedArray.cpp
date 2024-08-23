ece4eadd69aa2f1b639566f9d589919cff18b1d8
harryc73 <harry.connorsle@education.nsw.gov.au>
2024-08-23 11:22:16 +1000

    Updating repisotories

--- LED_Array/LedArray.cpp
+++ LED_Array/LedArray.cpp
@@ -7,8 +7,8 @@ ArduinoLEDMatrix matrix;
 LedArray::LedArray(const char* VersionNum)
 {
   this->_VersionNum = VersionNum;
+  this->status = status;
 }
-
 
 void LedArray::printVersion()
 {
@@ -19,4 +19,35 @@ void LedArray::printVersion()
   matrix.beginText(0, 1, 0xFF, 0, 0);
   matrix.print(_VersionNum);
   matrix.endText(SCROLL_LEFT);
+}
+
+void LedArray::updateStatus(const char* status)
+{
+  matrix.begin();
+  matrix.textFont(Font_5x7);
+  matrix.stroke(0xFF, 0, 0);
+  matrix.clear();  // Clear the display
+  matrix.beginText(0, 1, 0xFF, 0, 0);  // Set the position
+  matrix.print(status);
+  matrix.endText();
+}
+
+void LedArray::printGo()
+{
+  updateStatus("GO");
+}
+
+void LedArray::printLeft()
+{
+  updateStatus("<-");
+}
+
+void LedArray::printRight()
+{
+  updateStatus("->");
+}
+
+void LedArray::printBrake()
+{
+  updateStatus("BRAKE");
 }
