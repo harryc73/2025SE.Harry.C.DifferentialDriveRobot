3d364b52696271a41aa63b1535ab16b379cf3bff
harryc73 <harry.connorsle@education.nsw.gov.au>
2024-08-23 11:25:28 +1000

    Updating branch

---
+++ MotorControl/Motor.cpp
@@ -0,0 +1,88 @@
+#include "Motor.h"
+#include <Servo.h>
+
+
+Motor::Motor(byte leftPin, byte rightPin)
+  {
+    this->_pinLeft = leftPin;
+    this->_pinRight = rightPin;
+    goDelay = 50;
+    lastTimeGo = 0;
+    leftSpeed = leftWheelStopSpeed;
+    rightSpeed = rightWheelStopSpeed;
+  }
+
+
+Motor::Motor(byte leftPin, byte rightPin, unsigned long goDelay)
+  {
+    this->_pinLeft = leftPin;
+    this->_pinRight = rightPin;
+    this->goDelay = goDelay;
+    lastTimeGo = 0;
+    leftSpeed = leftWheelStopSpeed;
+    rightSpeed = rightWheelStopSpeed;
+  }
+
+  void Motor::init()
+  {
+    leftServo.attach(_pinLeft, 700, 2300);
+    rightServo.attach(_pinRight, 700, 2300);
+  }
+
+
+  void Motor::setMotors(unsigned int leftSpeed, unsigned int rightSpeed)
+  {
+    this->leftSpeed = leftSpeed;
+    this->rightSpeed = rightSpeed;
+  }
+
+  void Motor::update()
+  {
+    unsigned long currentTime = millis();
+    if (currentTime - lastTimeGo >= goDelay)
+    {
+      leftServo.writeMicroseconds(leftSpeed);
+      rightServo.writeMicroseconds(rightSpeed);
+      lastTimeGo = currentTime;
+  }
+  }
+
+  void Motor::accelerate()
+  {
+    setMotors(leftWheelGoSpeed, rightWheelGoSpeed);
+  }
+
+  void Motor::brake()
+  {
+    setMotors(leftWheelStopSpeed, rightWheelStopSpeed);
+  }
+
+  void Motor::reverse()
+  {
+    setMotors(leftWheelReverseSpeed, rightWheelReverseSpeed);
+  }
+
+  void Motor::turnLeft()
+  {
+    setMotors(leftWheelStopSpeed, rightWheelTurnSpeed);
+  }
+
+  void Motor::turnRight()
+  {
+    setMotors(leftWheelTurnSpeed, rightWheelStopSpeed);
+  }
+
+  void Motor::sharpTurnRight()
+  {
+    setMotors(leftWheelTurnSpeed, rightWheelReverseSpeed);
+  }
+
+  void Motor::sharpTurnLeft()
+  {
+    setMotors(leftWheelReverseSpeed, rightWheelTurnSpeed);
+  }
+
+  unsigned long Motor::getGoDelay()
+  {
+    return goDelay;
+  }
---
+++ MotorControl/Motor.h
@@ -0,0 +1,71 @@
+#ifndef MOTOR_H
+#define MOTOR_H
+
+#include <Servo.h>
+#include <Arduino.h>
+
+class Motor
+{
+private:
+  byte _pinLeft;
+  byte _pinRight;
+  Servo leftServo;
+  Servo rightServo;
+
+  unsigned int leftSpeed;
+  unsigned int rightSpeed;
+
+  // must add values here in here instead of cpp file to properly intitialise it for the R4 Board
+  // 1500 is stop
+  // Right: above 1500 is reverse and below 1500 is accelerate
+  // Left: above 1500 is accelrate and below 1500 is reverse
+  const unsigned int leftWheelGoSpeed = 1520;
+  const unsigned int rightWheelGoSpeed = 1490;
+
+  const unsigned int leftWheelStopSpeed = 1500;
+  const unsigned int rightWheelStopSpeed = 1500;
+
+  const unsigned int leftWheelReverseSpeed = 1430;
+  const unsigned int rightWheelReverseSpeed = 1560;
+
+  const unsigned int leftWheelTurnSpeed = 1720;
+  const unsigned int rightWheelTurnSpeed = 1300;
+
+
+
+  unsigned long lastTimeGo;
+  unsigned long goDelay;
+
+  void setMotors(unsigned int leftSpeed, unsigned int rightSpeed); // this assigns the values to be passed to the update method
+
+public:
+
+  Motor() {}
+
+  Motor(byte leftPin, byte rightPin);
+
+  Motor(byte leftPin, byte rightPin, unsigned long goDelay);
+
+  unsigned long getGoDelay();
+
+  void init();
+
+  void update(); 
+
+  void accelerate();
+
+  void brake();
+
+  void reverse();
+
+  void turnRight();
+
+  void turnLeft();
+
+  void sharpTurnLeft();
+
+  void sharpTurnRight();
+
+};
+
+#endif
